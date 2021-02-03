ac0ba9ff89ec4eee77708efae7a03db1afdfdc30
anthonyredmond <kushkurosaki@icloud.com>
2021-02-02 18:52:00 -0600

    assignment 2 part 2

--- src/org/launchcode/techjobs_oo/CoreCompetency.java
+++ src/org/launchcode/techjobs_oo/CoreCompetency.java
@@ -32,6 +32,9 @@ public class CoreCompetency {
         return id == that.id;
     }
 
+
+
+
     @Override
     public int hashCode() {
         return Objects.hash(id);
@@ -39,4 +42,17 @@ public class CoreCompetency {
 
     // TODO: Use the "Generate" tool to add a getter and setter for the 'value' field but
     //  ONLY a getter for the 'id' field.
+    public int getId() {
+        return id;
+    }
+
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
 }
